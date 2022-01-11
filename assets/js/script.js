026a4d215261f6f28397f6b09141f03d8fb2c387
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-01-11 17:01:34 +1030

    date shows on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
+const date = moment();
+
 // when user lands on page
 
 // user sees current date in header and business hours for that day in rows of one hour each - 9am - 5pm
 
-// date is that day and changes each day
+// timeblocks in form of table - 3 columns and 9 rows
 
-// when click on row can edit content inside with text
+// each row and hour of the day
+
+// col 1 = that time e.g 9am
+// col 2 = editable area (main width of row)
+// col 3 = save button
+
+// date is that day and changes each day
+$('#currentDay').text(date.format("dddd, MMMM Do"))
+
+// events can be added to each row
+
+// when click on row can edit content inside with text to add event
 
 // each row has a save button which will save content inside to local storage when clicked
 
