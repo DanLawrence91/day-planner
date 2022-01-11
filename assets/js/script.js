a891532b14e83e38670d75bc5c07396f3e575045
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-01-11 16:42:15 +1030

    comments added to js file

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// when user lands on page
+
+// user sees current date in header and business hours for that day in rows of one hour each - 9am - 5pm
+
+// date is that day and changes each day
+
+// when click on row can edit content inside with text
+
+// each row has a save button which will save content inside to local storage when clicked
+
+// data will be displayed next time page is shown 
+
+// each hour is color coded depending on whether in past, present or future. color will change automatically depending on current time
