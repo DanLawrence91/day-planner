635a4e2e4e39cf266c3a28941596d16182cfca9b
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-01-12 11:38:57 +1030

    table for timeblocks added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,15 @@
 const date = moment();
 
 // when user lands on page
-
 // user sees current date in header and business hours for that day in rows of one hour each - 9am - 5pm
+// date is that day and changes each day
+$('#currentDay').text(date.format("dddd, MMMM Do"))
 
 // timeblocks in form of table - 3 columns and 9 rows
-
 // each row and hour of the day
-
 // col 1 = that time e.g 9am
 // col 2 = editable area (main width of row)
 // col 3 = save button
-
-// date is that day and changes each day
-$('#currentDay').text(date.format("dddd, MMMM Do"))
 
 // events can be added to each row
 
